055ddfba9186bd4d138c81671ab6d47d1e92086c
Ivor Marić <6875970+IvorM@users.noreply.github.com>
2021-05-24 09:45:49 +0200

    Add project files.

---
+++ DelegatDemo/DelegatDemoClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatDemo
+{
+    class DelegatDemoClass
+    {
+        public delegate void TestDelegat();
+        private TestDelegat testDelegat;
+
+        public delegate void TestDelegat2(int i);
+        private TestDelegat2 testDelegat2;
+        public DelegatDemoClass()
+        {
+            testDelegat = MojaTestnaMetodaDelegata;
+            testDelegat();
+            testDelegat += MojaTestnaMetoda2Delegata;
+            testDelegat();
+            testDelegat2 += MojaTestnaMetoda3Delegata;
+            testDelegat2(3);
+            testDelegat -= MojaTestnaMetoda2Delegata;
+            testDelegat();
+            testDelegat += delegate () { Console.WriteLine("Anoninma funkcija"); };
+            testDelegat += () => { Console.WriteLine("Lambda"); };
+            testDelegat();
+        }
+
+        private void MojaTestnaMetodaDelegata()
+        {
+            Console.WriteLine("Metoda 1");
+        }
+
+        private void MojaTestnaMetoda2Delegata()
+        {
+            Console.WriteLine("Metoda 2");
+        }
+
+        private void MojaTestnaMetoda3Delegata(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+
+    }
+}
---
+++ DelegatDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DelegatDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegatDemoClass delegatDemoClass = new DelegatDemoClass();
+            Console.WriteLine("Unesite broj:");
+            var unsesniBroj = int.Parse(Console.ReadLine());
+            if (unsesniBroj==(int)EnumDemo.NaCekanju)
+            {
+                Console.WriteLine("Unesen je broj 0");
+            }
+        }
+    }
+}
